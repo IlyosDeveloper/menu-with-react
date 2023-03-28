a165d24a7c64205f8904c555b40f14c19cbccb5e
IlyosAxmadAliy <ilyosaxmadaliy@gmail.com>
2023-03-28 16:00:32 +0500

    /

--- src/pages/pageNotFount/Error.jsx
+++ src/pages/pageNotFount/Error.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Error() {
   return (
     <div className='title'>
-        <img src="../../../public/images/404-error-template-10.webp" alt="" width={800}/>
+        <img src="./images/404-error-template-10.webp" alt="page not found" width={800}/>
     </div>
   )
 }
